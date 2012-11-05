7edf888ee93d4e4eeb7b42f80cf378d982019e08
George Brindeiro <georgebrindeiro@lara.unb.br>
2012-11-05 16:51:46 -0200

    Hard-coded OST and SVS calibration parameters... WORKED!
    Important detail: mm -> m conversion needed in projection matrix coming
    from SVS (specifically, P[3] needs to be multiplied by 0.001).

--- videre_camera/src/videre_camera_node.cpp
+++ videre_camera/src/videre_camera_node.cpp
@@ -98,22 +98,173 @@ int main(int argc, char **argv)
     msg_left.encoding = sensor_msgs::image_encodings::BGR8;
     msg_right.encoding = sensor_msgs::image_encodings::BGR8;
 
+    // Warning that we are testing
+    ROS_INFO("Hard-coded calibration in videre_camera_node");
+
     // We can set camera info params prior to loop because they don't change
     info_left.height = vc->cp()->height();
     info_left.width = vc->cp()->width();
     info_left.distortion_model = vc->cp()->distortion_model();
-    vc->cp()->left_D(info_left.D);
-    vc->cp()->left_K(info_left.K);
-    vc->cp()->left_R(info_left.R);
-    vc->cp()->left_P(info_left.P);
+    //vc->cp()->left_D(info_left.D);
+    //vc->cp()->left_K(info_left.K);
+    //vc->cp()->left_R(info_left.R);
+    //vc->cp()->left_P(info_left.P);
+
+    /* OST Parameters */
+    /*info_left.D.push_back(-0.369133);
+    info_left.D.push_back(0.147212);
+    info_left.D.push_back(-0.002032);
+    info_left.D.push_back(0.002022);
+    info_left.D.push_back(0.000000);
+    info_left.K[0] = 292.585285;
+    info_left.K[1] = 0.000000;
+    info_left.K[2] = 159.848404;
+    info_left.K[3] = 0.000000;
+    info_left.K[4] = 292.076485;
+    info_left.K[5] = 106.437630;
+    info_left.K[6] = 0.000000;
+    info_left.K[7] = 0.000000;
+    info_left.K[8] = 1.000000;
+    info_left.R[0] = 0.997261;
+    info_left.R[1] = -0.002049;
+    info_left.R[2] = -0.073932;
+    info_left.R[3] = 0.002638;
+    info_left.R[4] = 0.999965;
+    info_left.R[5] = 0.007881;
+    info_left.R[6] = 0.073914;
+    info_left.R[7] = -0.008055;
+    info_left.R[8] = 0.997232;
+    info_left.P[0] = 304.389818;
+    info_left.P[1] = 0.000000;
+    info_left.P[2] = 184.028139;
+    info_left.P[3] = 0.000000;
+    info_left.P[4] = 0.000000;
+    info_left.P[5] = 304.389818;
+    info_left.P[6] = 112.227717;
+    info_left.P[7] = 0.000000;
+    info_left.P[8] = 0.000000;
+    info_left.P[9] = 0.000000;
+    info_left.P[10] = 1.000000;
+    info_left.P[11] = 0.000000;*/
+
+    /* SVS Parameters */
+    info_left.D.push_back(-0.283421);
+    info_left.D.push_back(-0.137791);
+    info_left.D.push_back(0.000000);
+    info_left.D.push_back(0.000000);
+    info_left.D.push_back(0.000000);
+    info_left.K[0] = 298.324130;
+    info_left.K[1] = 0.000000;
+    info_left.K[2] = 165.194788;
+    info_left.K[3] = 0.000000;
+    info_left.K[4] = 298.066606;
+    info_left.K[5] = 102.797277;
+    info_left.K[6] = 0.000000;
+    info_left.K[7] = 0.000000;
+    info_left.K[8] = 1.000000;
+    info_left.R[0] = 9.999927e-01;
+    info_left.R[1] = -3.466162e-03;
+    info_left.R[2] = -1.641216e-03;
+    info_left.R[3] = 3.468780e-03;
+    info_left.R[4] = 9.999927e-01;
+    info_left.R[5] = 1.594634e-03;
+    info_left.R[6] = 1.635677e-03;
+    info_left.R[7] = -1.600315e-03;
+    info_left.R[8] = 9.999974e-01;
+    info_left.P[0] = 2.980000e+02;
+    info_left.P[1] = 0.000000;
+    info_left.P[2] = 1.663312e+02;
+    info_left.P[3] = 0.000000;
+    info_left.P[4] = 0.000000;
+    info_left.P[5] = 2.980000e+02;
+    info_left.P[6] = 1.112314e+02;
+    info_left.P[7] = 0.000000;
+    info_left.P[8] = 0.000000;
+    info_left.P[9] = 0.000000;
+    info_left.P[10] = 1.000000;
+    info_left.P[11] = 0.000000;
 
     info_right.height = vc->cp()->height();
     info_right.width = vc->cp()->width();
     info_right.distortion_model = vc->cp()->distortion_model();
-    vc->cp()->right_D(info_right.D);
-    vc->cp()->right_K(info_right.K);
-    vc->cp()->right_R(info_right.R);
-    vc->cp()->right_P(info_right.P);
+    //vc->cp()->right_D(info_right.D);
+    //vc->cp()->right_K(info_right.K);
+    //vc->cp()->right_R(info_right.R);
+    //vc->cp()->right_P(info_right.P);
+
+    /* OST Parameters */
+    /*info_right.D.push_back(-0.361614);
+    info_right.D.push_back(0.141255);
+    info_right.D.push_back(0.000429);
+    info_right.D.push_back(-0.003258);
+    info_right.D.push_back(0.000000);
+    info_right.K[0] = 295.503697;
+    info_right.K[1] = 0.000000;
+    info_right.K[2] = 163.445080;
+    info_right.K[3] = 0.000000;
+    info_right.K[4] = 296.055638;
+    info_right.K[5] = 120.811073;
+    info_right.K[6] = 0.000000;
+    info_right.K[7] = 0.000000;
+    info_right.K[8] = 1.000000;
+    info_right.R[0] = 0.998604;
+    info_right.R[1] = 0.004671;
+    info_right.R[2] = -0.052616;
+    info_right.R[3] = -0.005091;
+    info_right.R[4] = 0.999956;
+    info_right.R[5] = -0.007842;
+    info_right.R[6] = 0.052577;
+    info_right.R[7] = 0.008099;
+    info_right.R[8] = 0.998584;
+    info_right.P[0] = 304.389818;
+    info_right.P[1] = 0.000000;
+    info_right.P[2] = 184.028139;
+    info_right.P[3] = -47.698098;
+    info_right.P[4] = 0.000000;
+    info_right.P[5] = 304.389818;
+    info_right.P[6] = 112.227717;
+    info_right.P[7] = 0.000000;
+    info_right.P[8] = 0.000000;
+    info_right.P[9] = 0.000000;
+    info_right.P[10] = 1.000000;
+    info_right.P[11] = 0.000000;*/
+
+    /* SVS Parameters */
+	info_right.D.push_back(-0.407392);
+	info_right.D.push_back(0.267797);
+	info_right.D.push_back(0.000000);
+	info_right.D.push_back(0.000000);
+	info_right.D.push_back(0.000000);
+	info_right.K[0] = 300.299950;
+	info_right.K[1] = 0.000000;
+	info_right.K[2] = 180.776418;
+	info_right.K[3] = 0.000000;
+	info_right.K[4] = 296.883585;
+	info_right.K[5] = 120.758520;
+	info_right.K[6] = 0.000000;
+	info_right.K[7] = 0.000000;
+	info_right.K[8] = 1.000000;
+	info_right.R[0] = 9.973115e-01;
+	info_right.R[1] = 2.665144e-03;
+	info_right.R[2] = 7.323095e-02;
+	info_right.R[3] = -2.548077e-03;
+	info_right.R[4] = 9.999954e-01;
+	info_right.R[5] = -1.691977e-03;
+	info_right.R[6] = -7.323512e-02;
+	info_right.R[7] = 1.500830e-03;
+	info_right.R[8] = 9.973136e-01;
+	info_right.P[0] = 2.980000e+02;
+	info_right.P[1] = 0.000000;
+	info_right.P[2] = 1.663312e+02;
+	info_right.P[3] = -4.632412e+04*0.001;
+	info_right.P[4] = 0.000000;
+	info_right.P[5] = 2.980000e+02;
+	info_right.P[6] = 1.112314e+02;
+	info_right.P[7] = 0.000000;
+	info_right.P[8] = 0.000000;
+	info_right.P[9] = 0.000000;
+	info_right.P[10] = 1.000000;
+	info_right.P[11] = 0.000000;
 
     // Set frame_ids
     msg_left.header.frame_id = "camera_left";
